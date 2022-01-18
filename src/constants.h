62bd6a210520bde2684da14c17fa99976903c5a8
Jason Carpenter <s@xvegx.com>
2022-01-18 13:04:21 -0500

    progress

---
+++ src/constants.h
@@ -0,0 +1,11 @@
+#define NUM_LEDS 27
+#define NUM_STRIPS 6
+#define MARQUEE_LENGTH 38
+#define FPS 5
+
+#define DATA_PIN_1 D1
+#define DATA_PIN_2 D2
+#define DATA_PIN_3 D3
+#define DATA_PIN_4 D4
+#define DATA_PIN_5 D5
+#define DATA_PIN_6 D6
