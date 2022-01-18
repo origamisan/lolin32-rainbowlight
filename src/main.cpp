68d3568292a2ad6f71ba3382dc4a04b1d3889706
Jason Carpenter <s@xvegx.com>
2022-01-18 13:03:22 -0500

    In Progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include "constants.h"
 
-#define NUM_LEDS 27
-#define NUM_STRIPS 6
-#define MARQUEE_LENGTH 38
-#define FPS 5
 
-#define DATA_PIN_1 D1
-#define DATA_PIN_2 D2
-#define DATA_PIN_3 D3
-#define DATA_PIN_4 D4
-#define DATA_PIN_5 D5
-#define DATA_PIN_6 D6
 
 // Define the array of leds
 CRGB leds[NUM_STRIPS][NUM_LEDS];
